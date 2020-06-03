9e2868a6b7078823963b7e6907d7c12e4667eb6b
chaihahaha <836275709@qq.com>
2020-06-03 17:43:15 +0200

    build dir

--- report_template/compile_report.cmd
+++ report_template/compile_report.cmd
@@ -1,4 +1,8 @@
-xelatex report_template
-bibtex report_template
-xelatex report_template
-xelatex report_template
+set build_dir=build
+set file_name=report_template
+if exist %build_dir% ( echo exist ) else ( mkdir %build_dir% )
+cd %build_dir%
+xelatex ../%file_name%
+bibtex %file_name%
+xelatex ../%file_name%
+xelatex ../%file_name%
